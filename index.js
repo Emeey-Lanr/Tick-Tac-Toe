41d9aebd2e1b5748e96e7c97bab3eac43b247564
Oyelowo Emmanuel <emeeylanr04@gmail.com>
2022-07-14 19:38:55 -1200

    I added an index html for the landing page for the game

---
+++ index.js
@@ -0,0 +1,17 @@
+const text = document.querySelector('.text');
+const btn = document.querySelector('.btnplayit')
+const bodyHousing = document.querySelector('.body');
+const playmusic = document.querySelector('#playsong');
+
+setTimeout(() => {
+    text.classList.remove('textNone')
+}, 2000)
+setTimeout(() => {
+    btn.classList.remove('btnplayNone')
+    bodyHousing.style.paddingBottom = '60px'
+
+}, 4000)
+btn.addEventListener('click', () => {
+    playmusic.play();
+})
+
